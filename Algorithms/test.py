eb414467556362c8724d5a8761f9a63de9aea052
SweeneyLi <35826417+Sweeney-Lee@users.noreply.github.com>
2018-08-30 09:45:32 +0800

    add 7

--- Algorithms/test.py
+++ Algorithms/test.py
@@ -1,25 +1,20 @@
 class Solution:
-    def new21Game(self, N, K, W):
-
-        dp = [0.0] * (N + W + 1)
-        # dp[x] = the answer when Alice has x points
-        for k in range(K, N + 1):
-            dp[k] = 1.0
-
-        S = min(N - K + 1, W)
-        # S = dp[k+1] + dp[k+2] + ... + dp[k+W]
-        for k in range(K - 1, -1, -1):
-            dp[k] = S / W
-            S += dp[k] - dp[k + W]
-
-        return dp[0]
-
+    def reverse(self, x):
         """
-        he key recursion is f(x) = (1/W) * (f(x+1) + f(x+2) + ... + f(x+W))
-        This is because there is a probability of \frac{1}{W} to draw each card from 1 to W
+        :type x: int
+        :rtype: int
         """
-s = Solution()
-print(s.new21Game(N=10, K=1, W=10))
-# print(s.new21Game(N=6, K=1, W=10))
-# print(s.new21Game(N=21, K=17, W=10))
-# print(s.new21Game(N = 0, K = 0, W = 1))
+        fu = False
+        if x < 0:
+            fu = True
+            x = -(x)
+            
+        st = str(x)
+        st1 = st[::-1]
+        result = int(st1)
+        
+        if result >> 31 != 0 :
+            result =  0
+        if fu:
+            result = - result
+        return result
