e57be5bc71d5496241bf7368d297d8f36790ad17
SweeneyLi <35826417+Sweeney-Lee@users.noreply.github.com>
2018-08-31 17:21:42 +0800

    update test

--- Algorithms/test.py
+++ Algorithms/test.py
@@ -1,4 +1,5 @@
 
+import functools
 class Solution:
     def letterCombinations(self, digits):
         """
@@ -7,26 +8,33 @@ class Solution:
         """
         ph_list = [[],[],['a', 'b', 'c'], ['d', 'e', 'f'],['g','h','i'],['j','k','l'], 
                    ['m','n','o'],['p', 'q', 'r', 's'],['t','u','v'],['w','x','y','z']]
+        key_list = [0,0,3,3,3,3,3,4,3,3,] 
+        change_list = [key_list[int(i)] for i in digits]
+        change_list.append(1)
+        print(change_list)
         dig_len = len(digits)
         result_len = 1
+        product = lambda x, y: x * y
         for i in digits:
-            result_len *= (4 if i == 9 or i == 7 else 3)
+            result_len *= key_list[i]
         
         result = ['' for i in range(result_len)]
         for i in range(result_len):
-            x = i
             # print(str(i) + ':')
             for j in range(dig_len):
-                if j == 0:
-                    m = x % (4 if digits[0] == '9' or digits[0] == '7' else 3)
-                    # print(x)
-                    result[i] += ph_list[int(digits[j])][m]
-                else :
-                    x = x // (4 if digits[j] == '9' or digits[j] == '7' else 3)
-                    # print(x)
-                    result[i] += ph_list[int(digits[j])][x]  
+                a = functools.reduce(product, change_list[j - 1])
+                b = functools.reduce(product, change_list[j - 1])
+                x = i % change_list[j]
+                result[i] += ph_list[int(digits[j])][x]  
                 print(result)
         return result
+
+# alist = functools.reduce(lambda x, y: x + y, [1, 3, 5, 7, 9])
+# print(alist)
+s = Solution()
+a = s.letterCombinations('2743')
+print(a)
+
 s = Solution()
 a = s.letterCombinations('234')
 print(a)
