3c704fcd8fbc282dd477e7f7e4993fc5fad5962c
SweeneyLi <sweeney.x.li@pwc.com>
2018-10-16 16:34:56 +0800

    finish 121

--- temp.py
+++ temp.py
@@ -1,7 +1,17 @@
-
-
-
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        result = 0
+        temp = 0
+        for i in range(1, len(prices)):
+            temp += prices[i] - prices[i - 1]
+            temp = temp if temp >= 0 else 0
+            result = max(temp, result)
+        return result
 
 s = Solution()
-a = s.
+a = s.maxProfit([7,6,4,3,1])
 print(a)
