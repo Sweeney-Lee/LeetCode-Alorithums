e9f537cce0e70c79dacc7feb121a33d7169d80e1
SweeneyLi <sweeney.x.li@pwc.com>
2018-10-18 15:09:25 +0800

    finish 198

--- temp.py
+++ temp.py
@@ -1,4 +1,17 @@
+class Solution:
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        rob = 0
+        notrob = 0
+        for i in range(len(nums)):
+            currob = notrob + nums[i]
+            notrob = max(notrob, rob)
+            rob = currob
+        return max(rob, notrob)
 
 s = Solution()
-a = s
+a = s.rob([2,7,9,3,1])
 print(a)
