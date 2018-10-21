301a7034059953d6a8d6a6e29a3edcf742c47cd1
Sweeney-Lee <sweeneylee@qq.com>
2018-10-21 16:26:15 +0800

    review questions

--- temp.py
+++ temp.py
@@ -1,5 +1,22 @@
-
+from bisect import bisect_left
+class Solution:
+    def lengthOfLIS(self, nums):
+        if not nums: return 0
+        dp = [0 for _ in range(len(nums))]
+        dp[0] = nums[0]
+        length = 1
+        for index in range(1, len(nums)):
+            num = nums[index]
+            if num < dp[0]:
+                dp[0] = num
+            elif num > dp[length-1]:
+                dp[length] = num
+                length += 1
+            else:
+                i = bisect_left(dp, num, 0, length)
+                dp[i] = num
+        return length
 
 s = Solution()
-a = s.rob([2,7,9,3,1])
+a = s.lengthOfLIS([10,9,2,5,3,7,101,18])
 print(a)
