ce54afe69ea023a507ed55298f1f47fe4e34b879
Sweeney-Lee <sweeneylee@qq.com>
2018-10-27 10:40:19 +0800

    clear the temp

--- temp.py
+++ temp.py
@@ -1,22 +1,6 @@
-class Solution:
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        if not s:
-            return True
-        a = [s[0]]
-        i = 1
-        while i < len(s):
-            if s[i] in ['(', '{', '[']:
-                a.append(s[i])
-            elif len(a) >= 1 and ((s[i] == ')' and a[-1] == '(') or (s[i] == '}' and a[-1] == '{') or (s[i] == ']' and a[-1] == '[')):
-                    a.pop()
-            else:
-                return False
-            i += 1
-        return True
+
+
+
 s = Solution()
 a = s.isValid("{}][}}{[))){}{}){(}]))})[({")
 print(a)
